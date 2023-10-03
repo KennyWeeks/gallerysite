b5ba5fbfccc69904227f0bd89b9b91f4b6faadf0
Kenny Weeks <weeksgit@gmail.com>
2023-10-02 22:20:08 -0700

    A parse string, nothing crazy.

--- app/components/imagecomponent.tsx
+++ app/components/imagecomponent.tsx
@@ -16,13 +16,26 @@ const ImageComp : React.FC<ImageComp> = ({imageUrl}) => {
         })
     },  {threshold: 0.3})
 
+    const parseString = (imagePath : string) => {
+        //So we will decide whether to make the image portrait or landscape based on if the image has a 'l' in the path
+        let path : string = imagePath.split(".")[0];
+
+        if(path.charAt(path.length - 1) == 'l') {
+            //Landscape
+            return 600;
+        } else {
+            return 300;
+        }
+
+    }
+
     return(
         <div className={"inline-block"}>
             {/*This will determine whether the final image is visible or invisible, which is trigger by the page tracker
             idk how this will essentially work, cause each of these images has a render component, and I want to affect each of these individually.
             It feels like this may be really bad cause I will have multiple window observers acting with each other, but we'll see how that works eventually*/}
             {/*<div ref={cbRef} className={"bg-[url('" + imageUrl + "')] w-[200px] h-[200px] " + (visible ? "visible": "invisible")}></div>*/}
-            <Image ref={cbRef} src={imageUrl} alt={"Image Here"} width={300} height={400} className={"transition-op duration-1000 " + (visible ? "opacity-1" : "opacity-0")}/>
+            <Image ref={cbRef} src={imageUrl} alt={"Image Here"} width={parseString(imageUrl)} height={400} className={"transition-op duration-1000 " + (visible ? "opacity-1" : "opacity-0")}/>
             
         </div>
     );
